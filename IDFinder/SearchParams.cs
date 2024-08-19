9f0e11648ced13350471ecbb11c7e2411fb48d83
Eb3yr <adamkjohnson123@gmail.com>
2024-08-19 01:56:40 +0100

    Implemented interfaces for scav search params. Conflicted on whether I love or hate this.

--- IDFinder/SearchParams.cs
+++ IDFinder/SearchParams.cs
@@ -1,15 +1,10 @@
 ﻿using System.Reflection;
+using static IDFinder.BackDecals;
+using static IDFinder.BackTuftsAndRidges;
 
 namespace IDFinder
 {
 	#region classes
-	// How do I weigh multiple at once? Does this method allow for that? Could I dynamically generate a class that implements these for everything wanted by the user, to use in the searching algorithm? 
-	// Some kind of compound search method in the base Search class that takes any number of searchparam class as arguments and calls the weight functions for each?
-	// Should the weight functions be made static to facilitate this? Currently SearchParams are a property of the searching class, but a reference could be passed instead.
-	// Tbh I think each interface should have its own class, and then the SlugParams, ScavParams etc have instances of those. It'll play nice with the website GUI
-	// I can cast SlugParams down to its individual interfaces, and then pass those individuals to the weighting methods. This means I can take any Set of ISearchParams, iterate through it once to grab the types within it, assign to interface-type variables, and pass those as arguments to static weighting methods.
-
-
 	public class SearchParams : ISearchParams, IPersonalityParams, INPCStatsParams, ISlugcatStatsParams, IFoodPreferencesParams, IIndividualVariationsParams, IEartlersParams, IScavColorsParams, IScavSkillsParams, IScavBackPatternsParams
 	{
 		public (float target, float weight)? Sympathy { get; set; }
@@ -55,7 +50,59 @@ namespace IDFinder
 		public (float target, float weight)? SmallNeedleWorm { get; set; }
 		public (float target, float weight)? Hazer { get; set; }
 		public (float target, float weight)? NotCounted { get; set; }
-		public bool Elite { get; set; }
+		public (bool target, float weight)? Elite { get; set; }
+		public (float target, float weight)? WaistWidth { get; set; }
+		public (float target, float weight)? HeadSize { get; set; }
+		public (float target, float weight)? EartlerWidth { get; set; }
+		public (float target, float weight)? NeckThickness { get; set; }
+		public (float target, float weight)? HandsHeadColor { get; set; }
+		public (float target, float weight)? EyeSize { get; set; }
+		public (float target, float weight)? NarrowEyes { get; set; }
+		public (float target, float weight)? EyesAngle { get; set; }
+		public (float target, float weight)? Fatness { get; set; }
+		public (float target, float weight)? NarrowWaist { get; set; }
+		public (float target, float weight)? LegsSize { get; set; }
+		public (float target, float weight)? ArmThickness { get; set; }
+		public (float target, float weight)? WideTeeth { get; set; }
+		public (float target, float weight)? PupilSize { get; set; }
+		public (float target, float weight)? Scruffy { get; set; }
+		public (bool target, float weight)? ColoredEartlerTips { get; set; }
+		public (bool target, float weight)? DeepPupils { get; set; }
+		public (int target, float weight)? ColoredPupils { get; set; }
+		public (int target, float weight)? TailSegs { get; set; }
+		public (float target, float weight)? GeneralMelanin { get; set; }
+		public (float target, float weight)? BellyColorH { get; set; }
+		public (float target, float weight)? BellyColorS { get; set; }
+		public (float target, float weight)? BellyColorL { get; set; }
+		public (float target, float weight)? BodyColorH { get; set; }
+		public (float target, float weight)? BodyColorS { get; set; }
+		public (float target, float weight)? BodyColorL { get; set; }
+		public (float target, float weight)? DecorationColorH { get; set; }
+		public (float target, float weight)? DecorationColorS { get; set; }
+		public (float target, float weight)? DecorationColorL { get; set; }
+		public (float target, float weight)? EyeColorH { get; set; }
+		public (float target, float weight)? EyeColorL { get; set; }
+		public (float target, float weight)? HeadColorH { get; set; }
+		public (float target, float weight)? HeadColorS { get; set; }
+		public (float target, float weight)? HeadColorL { get; set; }
+		public (float target, float weight)? BellyColorBlack { get; set; }
+		public (float target, float weight)? BodyColorBlack { get; set; }
+		public (float target, float weight)? HeadColorBlack { get; set; }
+		public (float target, float weight)? BlockingSkill { get; set; }
+		public (float target, float weight)? DodgeSkill { get; set; }
+		public (float target, float weight)? MeleeSkill { get; set; }
+		public (float target, float weight)? MidRangeSkill { get; set; }
+		public (float target, float weight)? ReactionSkill { get; set; }
+		public (float target, float weight)? Top { get; set; }
+		public (float target, float weight)? Bottom { get; set; }
+		public (BackPattern target, float weight)? Pattern { get; set; }
+		public (BackDecals target, float weight)? Type { get; set; }
+		public (ColorTypeEnum target, float weight)? ColorType { get; set; }
+		public (bool target, float weight)? IsColored { get; set; }
+		public (int target, float weight)? ScaleGraf { get; set; }
+		public (float target, float weight)? GeneralSize { get; set; }
+		public (float target, float weight)? Colored { get; set; }
+		public (int target, float weight)? NumberOfSpines { get; set; }
 	}
 	public class SlugParams : IPersonalityParams, INPCStatsParams, ISlugcatStatsParams, IFoodPreferencesParams
 	{
@@ -105,13 +152,66 @@ namespace IDFinder
 	}
 	public class ScavParams : IPersonalityParams, IIndividualVariationsParams, IEartlersParams, IScavColorsParams, IScavSkillsParams, IScavBackPatternsParams
 	{
-		public (float target, float weight)? Sympathy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-		public (float target, float weight)? Energy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-		public (float target, float weight)? Bravery { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-		public (float target, float weight)? Nervous { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-		public (float target, float weight)? Aggression { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-		public (float target, float weight)? Dominance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-		public bool Elite { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public (float target, float weight)? Sympathy { get; set; }
+		public (float target, float weight)? Energy { get; set; }
+		public (float target, float weight)? Bravery { get; set; }
+		public (float target, float weight)? Nervous { get; set; }
+		public (float target, float weight)? Aggression { get; set; }
+		public (float target, float weight)? Dominance { get; set; }
+		public bool Elite { get; set; }
+		public (float target, float weight)? WaistWidth { get; set; }
+		public (float target, float weight)? HeadSize { get; set; }
+		public (float target, float weight)? EartlerWidth { get; set; }
+		public (float target, float weight)? NeckThickness { get; set; }
+		public (float target, float weight)? HandsHeadColor { get; set; }
+		public (float target, float weight)? EyeSize { get; set; }
+		public (float target, float weight)? NarrowEyes { get; set; }
+		public (float target, float weight)? EyesAngle { get; set; }
+		public (float target, float weight)? Fatness { get; set; }
+		public (float target, float weight)? NarrowWaist { get; set; }
+		public (float target, float weight)? LegsSize { get; set; }
+		public (float target, float weight)? ArmThickness { get; set; }
+		public (float target, float weight)? WideTeeth { get; set; }
+		public (float target, float weight)? PupilSize { get; set; }
+		public (float target, float weight)? Scruffy { get; set; }
+		public (bool target, float weight)? ColoredEartlerTips { get; set; }
+		public (bool target, float weight)? DeepPupils { get; set; }
+		public (int target, float weight)? ColoredPupils { get; set; }
+		public (int target, float weight)? TailSegs { get; set; }
+		public (float target, float weight)? GeneralMelanin { get; set; }
+		public (float target, float weight)? BellyColorH { get; set; }
+		public (float target, float weight)? BellyColorS { get; set; }
+		public (float target, float weight)? BellyColorL { get; set; }
+		public (float target, float weight)? BodyColorH { get; set; }
+		public (float target, float weight)? BodyColorS { get; set; }
+		public (float target, float weight)? BodyColorL { get; set; }
+		public (float target, float weight)? DecorationColorH { get; set; }
+		public (float target, float weight)? DecorationColorS { get; set; }
+		public (float target, float weight)? DecorationColorL { get; set; }
+		public (float target, float weight)? EyeColorH { get; set; }
+		public (float target, float weight)? EyeColorL { get; set; }
+		public (float target, float weight)? HeadColorH { get; set; }
+		public (float target, float weight)? HeadColorS { get; set; }
+		public (float target, float weight)? HeadColorL { get; set; }
+		public (float target, float weight)? BellyColorBlack { get; set; }
+		public (float target, float weight)? BodyColorBlack { get; set; }
+		public (float target, float weight)? HeadColorBlack { get; set; }
+		public (float target, float weight)? BlockingSkill { get; set; }
+		public (float target, float weight)? DodgeSkill { get; set; }
+		public (float target, float weight)? MeleeSkill { get; set; }
+		public (float target, float weight)? MidRangeSkill { get; set; }
+		public (float target, float weight)? ReactionSkill { get; set; }
+		public (float target, float weight)? Top { get; set; }
+		public (float target, float weight)? Bottom { get; set; }
+		public (BackPattern target, float weight)? Pattern { get; set; }
+		public (BackDecals target, float weight)? Type { get; set; }
+		public (ColorTypeEnum target, float weight)? ColorType { get; set; }
+		public (bool target, float weight)? IsColored { get; set; }
+		public (int target, float weight)? ScaleGraf { get; set; }
+		public (float target, float weight)? GeneralSize { get; set; }
+		public (float target, float weight)? Colored { get; set; }
+		public (int target, float weight)? NumberOfSpines { get; set; }
+
 		public ScavParams() => throw new NotImplementedException("Scavenger parameters are not yet implemented.");
 	}
 	public class NewSlugParams()
@@ -270,23 +370,72 @@ namespace IDFinder
 
     public interface IIndividualVariationsParams : ISearchParams
     {
-        public bool Elite { get; set; }
-    }
+		public (float target, float weight)? WaistWidth { get; set; }
+		public (float target, float weight)? HeadSize { get; set; }
+		public (float target, float weight)? EartlerWidth { get; set; }
+		public (float target, float weight)? NeckThickness { get; set; }
+		public (float target, float weight)? HandsHeadColor { get; set; }
+		public (float target, float weight)? EyeSize { get; set; }
+		public (float target, float weight)? NarrowEyes { get; set; }
+		public (float target, float weight)? EyesAngle { get; set; }
+		public (float target, float weight)? Fatness { get; set; }
+		public (float target, float weight)? NarrowWaist { get; set; }
+		public (float target, float weight)? LegsSize { get; set; }
+		public (float target, float weight)? ArmThickness { get; set; }
+		public (float target, float weight)? WideTeeth { get; set; }
+		public (float target, float weight)? PupilSize { get; set; }
+		public (float target, float weight)? Scruffy { get; set; }
+		public (bool target, float weight)? ColoredEartlerTips { get; set; }
+		public (bool target, float weight)? DeepPupils { get; set; }
+		public (int target, float weight)? ColoredPupils { get; set; }
+		public (int target, float weight)? TailSegs { get; set; }
+		public (float target, float weight)? GeneralMelanin { get; set; }
+	}
     public interface IEartlersParams : ISearchParams
     {
         // Does this even need to be visible? The data itself is quite abstracted from its in-game appearance. It could be searched for some more interesting observations though, like eartler size, complexity, etc. Needs to be thought over and experimented with ingame. 
     }
     public interface IScavColorsParams : ISearchParams
-    { 
-        
-    }
+    {
+		// I think this is better than the alternative of HSLColor structs, because it'd be convoluted figuring out which color channels to check. 
+		public (float target, float weight)? BellyColorH { get; set; }
+		public (float target, float weight)? BellyColorS { get; set; }
+		public (float target, float weight)? BellyColorL { get; set; }
+		public (float target, float weight)? BodyColorH { get; set; }
+		public (float target, float weight)? BodyColorS { get; set; }
+		public (float target, float weight)? BodyColorL { get; set; }
+		public (float target, float weight)? DecorationColorH { get; set; }
+		public (float target, float weight)? DecorationColorS { get; set; }
+		public (float target, float weight)? DecorationColorL { get; set; }
+		public (float target, float weight)? EyeColorH { get; set; }
+		public (float target, float weight)? EyeColorL { get; set; }
+		public (float target, float weight)? HeadColorH { get; set; }
+		public (float target, float weight)? HeadColorS { get; set; }
+		public (float target, float weight)? HeadColorL { get; set; }
+		public (float target, float weight)? BellyColorBlack { get; set; }
+		public (float target, float weight)? BodyColorBlack { get; set; }
+		public (float target, float weight)? HeadColorBlack { get; set; }
+	}
     public interface IScavSkillsParams : ISearchParams
     {
-
-    }
+		public (float target, float weight)? BlockingSkill { get; set; }
+		public (float target, float weight)? DodgeSkill { get; set; }
+		public (float target, float weight)? MeleeSkill { get; set; }
+		public (float target, float weight)? MidRangeSkill { get; set; }
+		public (float target, float weight)? ReactionSkill { get; set; }
+	}
     public interface IScavBackPatternsParams : ISearchParams
     {
-        // Remember that this isn't a 1-to-1 recreation of the properties & fields of the respective classes. The information presented needs to be useful to the user, an array of spine sizes is less useful than an averaging of the spine sizes and another for the total number of spines. 
-    }
+		public (float target, float weight)? Top { get; set; }
+		public (float target, float weight)? Bottom { get; set; }
+		public (BackPattern target, float weight)? Pattern { get; set; }
+		public (BackDecals target, float weight)? Type { get; set; }
+		public (ColorTypeEnum target, float weight)? ColorType { get; set; }
+		public (bool target, float weight)? IsColored { get; set; }
+		public (int target, float weight)? ScaleGraf { get; set; }
+		public (float target, float weight)? GeneralSize { get; set; }
+		public (float target, float weight)? Colored { get; set; }
+		public (int target, float weight)? NumberOfSpines { get; set; }
+	}
     #endregion
 }
