841b4ec8448bc057c480fa4fb27ad61136e08c2a
Eb3yr <adamkjohnson123@gmail.com>
2024-08-20 03:46:20 +0100

    Removed use of reflection to check if properties are null, removed abstract AbstractSearchParams class that had the reflection-based AllNull() method. Now each interface that inherits ISearchParams has its own AllNull() implementation.

--- IDFinder/SearchParams.cs
+++ IDFinder/SearchParams.cs
@@ -5,7 +5,8 @@ using static IDFinder.BackTuftsAndRidges;
 namespace IDFinder
 {
 	#region classes
-	public class SearchParams : ISearchParams, IPersonalityParams, INPCStatsParams, ISlugcatStatsParams, IFoodPreferencesParams, IIndividualVariationsParams, IEartlersParams, IScavColorsParams, IScavSkillsParams, IScavBackPatternsParams
+	// Add implicit casts to SearchParams from SlugParams and ScavParams. That way the latter can be passed as a parameter without breaking anything in Search.cs
+	public class SearchParams : ISearchParams, IPersonalityParams, INPCStatsParams, ISlugcatStatsParams, IFoodPreferencesParams, IIndividualVariationsParams, IEartlersParams, IScavColorsParams, IScavSkillsParams, IScavBackPatternsParams	 // H, S, L should be more clear that it's slugcat npcstats. Likewise for some others. So long as everything has unique names no bugs should arise, but I wouldn't recommend directly assigning properties in this class, rather use SlugParams, ScavParams etc once I implement operators. 
 	{
 		public (float target, float weight)? Sympathy { get; set; }
 		public (float target, float weight)? Energy { get; set; }
@@ -50,6 +51,7 @@ namespace IDFinder
 		public (float target, float weight)? SmallNeedleWorm { get; set; }
 		public (float target, float weight)? Hazer { get; set; }
 		public (float target, float weight)? NotCounted { get; set; }
+		public bool Elite { get; set; } = false;
 		public (float target, float weight)? WaistWidth { get; set; }
 		public (float target, float weight)? HeadSize { get; set; }
 		public (float target, float weight)? EartlerWidth { get; set; }
@@ -95,15 +97,28 @@ namespace IDFinder
 		public (float target, float weight)? Top { get; set; }
 		public (float target, float weight)? Bottom { get; set; }
 		public (BackPattern target, float weight)? Pattern { get; set; }
-		public (BackDecals target, float weight)? Type { get; set; }
+		public (string target, float weight)? Type { get; set; }
 		public (ColorTypeEnum target, float weight)? ColorType { get; set; }
 		public (bool target, float weight)? IsColored { get; set; }
 		public (int target, float weight)? ScaleGraf { get; set; }
 		public (float target, float weight)? GeneralSize { get; set; }
 		public (float target, float weight)? Colored { get; set; }
 		public (int target, float weight)? NumberOfSpines { get; set; }
+		public bool AllNull()
+		{
+			return
+				((IPersonalityParams)this).AllNull() &&
+				((INPCStatsParams)this).AllNull() &&
+				((ISlugcatStatsParams)this).AllNull() &&
+				((IFoodPreferencesParams)this).AllNull() &&
+				((IIndividualVariationsParams)this).AllNull() &&
+				// Eartlers
+				((IScavColorsParams)this).AllNull() &&
+				((IScavSkillsParams)this).AllNull() &&
+				((IScavBackPatternsParams)this).AllNull();
+		}
 	}
-	public class SlugParams : IPersonalityParams, INPCStatsParams, ISlugcatStatsParams, IFoodPreferencesParams
+	public class SlugParams : ISearchParams, IPersonalityParams, INPCStatsParams, ISlugcatStatsParams, IFoodPreferencesParams
 	{
 		public (float target, float weight)? Sympathy { get; set; }
 		public (float target, float weight)? Energy { get; set; }
@@ -148,8 +163,16 @@ namespace IDFinder
 		public (float target, float weight)? SmallNeedleWorm { get; set; }
 		public (float target, float weight)? Hazer { get; set; }
 		public (float target, float weight)? NotCounted { get; set; }
+		public bool AllNull()
+		{
+			return
+				((IPersonalityParams)this).AllNull() &&
+				((INPCStatsParams)this).AllNull() &&
+				((ISlugcatStatsParams)this).AllNull() &&
+				((IFoodPreferencesParams)this).AllNull();
+		}
 	}
-	public class ScavParams : IPersonalityParams, IIndividualVariationsParams, IEartlersParams, IScavColorsParams, IScavSkillsParams, IScavBackPatternsParams
+	public class ScavParams : ISearchParams, IPersonalityParams, IIndividualVariationsParams, IEartlersParams, IScavColorsParams, IScavSkillsParams, IScavBackPatternsParams
 	{
 		public (float target, float weight)? Sympathy { get; set; }
 		public (float target, float weight)? Energy { get; set; }
@@ -157,6 +180,7 @@ namespace IDFinder
 		public (float target, float weight)? Nervous { get; set; }
 		public (float target, float weight)? Aggression { get; set; }
 		public (float target, float weight)? Dominance { get; set; }
+		public bool Elite { get; set; } = false;
 		public (float target, float weight)? WaistWidth { get; set; }
 		public (float target, float weight)? HeadSize { get; set; }
 		public (float target, float weight)? EartlerWidth { get; set; }
@@ -202,112 +226,29 @@ namespace IDFinder
 		public (float target, float weight)? Top { get; set; }
 		public (float target, float weight)? Bottom { get; set; }
 		public (BackPattern target, float weight)? Pattern { get; set; }
-		public (BackDecals target, float weight)? Type { get; set; }
+		public (string target, float weight)? Type { get; set; }
 		public (ColorTypeEnum target, float weight)? ColorType { get; set; }
 		public (bool target, float weight)? IsColored { get; set; }
 		public (int target, float weight)? ScaleGraf { get; set; }
 		public (float target, float weight)? GeneralSize { get; set; }
 		public (float target, float weight)? Colored { get; set; }
 		public (int target, float weight)? NumberOfSpines { get; set; }
-
-		public ScavParams() => throw new NotImplementedException("Scavenger parameters are not yet implemented.");
+		public bool AllNull()
+		{
+			return
+				((IPersonalityParams)this).AllNull() &&
+				((IIndividualVariationsParams)this).AllNull() &&
+				// Eartlers
+				((IScavColorsParams)this).AllNull() &&
+				((IScavSkillsParams)this).AllNull() &&
+				((IScavBackPatternsParams)this).AllNull();
+		}
 	}
-	public class NewSlugParams()
-	{
-		public PersonalityParams  PersonalityParams { get; set; } = new();
-		public NPCStatsParams NPCStatsParams { get; set; } = new();
-		public SlugcatStatsParams SlugcatStatsParams { get; set; } = new();
-		public FoodPreferencesParams FoodPreferencesParams{ get; set; } = new();
-	}
-	
-	public class PersonalityParams : ISearchParams
-	{
-		public (float target, float weight)? Sympathy { get; set; }
-		public (float target, float weight)? Energy { get; set; }
-		public (float target, float weight)? Bravery { get; set; }
-		public (float target, float weight)? Nervous { get; set; }
-		public (float target, float weight)? Aggression { get; set; }
-		public (float target, float weight)? Dominance { get; set; }
-	}
-	public class NPCStatsParams : ISearchParams
-	{
-		public (float target, float weight)? Met { get; set; }
-		public (float target, float weight)? Bal { get; set; }
-		public (float target, float weight)? Size { get; set; }
-		public (float target, float weight)? Stealth { get; set; }
-		public (bool target, float weight)? Dark { get; set; }
-		public (float target, float weight)? EyeColor { get; set; }
-		public (float target, float weight)? H { get; set; }
-		public (float target, float weight)? S { get; set; }
-		public (float target, float weight)? L { get; set; }
-		public (float target, float weight)? Wideness { get; set; }
-	}
-	public class SlugcatStatsParams : ISearchParams
-	{
-		public (float target, float weight)? BodyWeightFac { get; set; }
-		public (float target, float weight)? GeneralVisibilityBonus { get; set; }
-		public (float target, float weight)? VisualStealthInSneakMode { get; set; }
-		public (float target, float weight)? LoudnessFac { get; set; }
-		public (float target, float weight)? LungsFac { get; set; }
-		public (int target, float weight)? ThrowingSkill { get; set; }
-		public (float target, float weight)? PoleClimbSpeedFac { get; set; }
-		public (float target, float weight)? CorridorClimbSpeedFac { get; set; }
-		public (float target, float weight)? RunSpeedFac { get; set; }
-	}
-	public class FoodPreferencesParams : ISearchParams
-	{
-		public (float target, float weight)? DangleFruit { get; set; }
-		public (float target, float weight)? WaterNut { get; set; }
-		public (float target, float weight)? JellyFish { get; set; }
-		public (float target, float weight)? SlimeMold { get; set; }
-		public (float target, float weight)? EggBugEgg { get; set; }
-		public (float target, float weight)? FireEgg { get; set; }
-		public (float target, float weight)? Popcorn { get; set; }
-		public (float target, float weight)? GooieDuck { get; set; }
-		public (float target, float weight)? LilyPuck { get; set; }
-		public (float target, float weight)? GlowWeed { get; set; }
-		public (float target, float weight)? DandelionPeach { get; set; }
-		public (float target, float weight)? Neuron { get; set; }
-		public (float target, float weight)? Centipede { get; set; }
-		public (float target, float weight)? SmallCentipede { get; set; }
-		public (float target, float weight)? VultureGrub { get; set; }
-		public (float target, float weight)? SmallNeedleWorm { get; set; }
-		public (float target, float weight)? Hazer { get; set; }
-		public (float target, float weight)? NotCounted { get; set; }
-	}
-
-	public class IndividualVariationsParams : ISearchParams
-	{
-	}
-	public class EartlersParams : ISearchParams
-	{
-		// Does this even need to be visible? The data itself is quite abstracted from its in-game appearance. It could be searched for some more interesting observations though, like eartler size, complexity, etc. Needs to be thought over and experimented with ingame. 
-	}
-	public class ScavColorsParams : ISearchParams
-	{
-
-	}
-	public class ScavSkillsParams : ISearchParams
-	{
-
-	}
-	public class ScavBackPatternsParams : ISearchParams
-	{
-		// Remember that this isn't a 1-to-1 recreation of the properties & fields of the respective classes. The information presented needs to be useful to the user, an array of spine sizes is less useful than an averaging of the spine sizes and another for the total number of spines. 
-	}
-
 	#endregion
 	#region interfaces
 	public interface ISearchParams
 	{
-		public bool AllNull()
-		{
-			foreach (PropertyInfo pi in this.GetType().GetProperties())
-				if (pi.GetValue(this) is not null)
-					return false;
-
-			return true;
-		}
+		public abstract bool AllNull();
 	}
     public interface IPersonalityParams : ISearchParams
     {
@@ -317,6 +258,18 @@ namespace IDFinder
         public (float target, float weight)? Nervous { get; set; }
         public (float target, float weight)? Aggression { get; set; }
         public (float target, float weight)? Dominance { get; set; }
+		public new bool AllNull()
+		{
+			if (Sympathy is null &&
+				Energy is null &&
+				Bravery is null &&
+				Nervous is null &&
+				Aggression is null &&
+				Dominance is null)
+				return true;
+
+			return false;
+		}
     }
     public interface INPCStatsParams : ISearchParams
     {
@@ -330,6 +283,22 @@ namespace IDFinder
         public (float target, float weight)? S { get; set; }
         public (float target, float weight)? L { get; set; }
         public (float target, float weight)? Wideness { get; set; }
+		public new bool AllNull()
+		{
+			if (Met is null &&
+				Bal is null &&
+				Size is null &&
+				Stealth is null &&
+				Dark is null &&
+				EyeColor is null &&
+				H is null &&
+				S is null &&
+				L is null &&
+				Wideness is null)
+				return true;
+
+			return false;
+		}
     }
     public interface ISlugcatStatsParams : ISearchParams
     {
@@ -342,6 +311,21 @@ namespace IDFinder
         public (float target, float weight)? PoleClimbSpeedFac { get; set; }
         public (float target, float weight)? CorridorClimbSpeedFac { get; set; }
         public (float target, float weight)? RunSpeedFac { get; set; }
+		public new bool AllNull()
+		{
+			if (BodyWeightFac is null &&
+				GeneralVisibilityBonus is null &&
+				VisualStealthInSneakMode is null &&
+				LoudnessFac is null &&
+				LungsFac is null &&
+				ThrowingSkill is null &&
+				PoleClimbSpeedFac is null &&
+				CorridorClimbSpeedFac is null &&
+				RunSpeedFac is null)
+				return true;
+
+			return false;
+		}
     }
     public interface IFoodPreferencesParams : ISearchParams
     {
@@ -363,11 +347,36 @@ namespace IDFinder
         public (float target, float weight)? SmallNeedleWorm { get; set; }
         public (float target, float weight)? Hazer { get; set; }
         public (float target, float weight)? NotCounted { get; set; }
+		public new bool AllNull()
+		{
+			if (DangleFruit is null &&
+				WaterNut is null &&
+				JellyFish is null &&
+				SlimeMold is null &&
+				EggBugEgg is null &&
+				FireEgg is null &&
+				Popcorn  is null &&
+				GooieDuck is null &&
+				LilyPuck is null &&
+				GlowWeed is null &&
+				DandelionPeach  is null &&
+				Neuron is null &&
+				Centipede is null &&
+				SmallCentipede is null &&
+				VultureGrub is null &&
+				SmallNeedleWorm is null &&
+				Hazer is null &&
+				NotCounted is null)
+				return true;
+
+			return false;
+		}
     }
 
     public interface IIndividualVariationsParams : ISearchParams
     {
 		// Elites not here as it's a parameter in the Scavenger constructor, and influences other traits. Like slugcats and scavs are different, scavs and elite scavs are also different.
+		public bool Elite { get; set; }
 		public (float target, float weight)? WaistWidth { get; set; }
 		public (float target, float weight)? HeadSize { get; set; }
 		public (float target, float weight)? EartlerWidth { get; set; }
@@ -388,6 +397,32 @@ namespace IDFinder
 		public (int target, float weight)? ColoredPupils { get; set; }
 		public (int target, float weight)? TailSegs { get; set; }
 		public (float target, float weight)? GeneralMelanin { get; set; }
+		public new bool AllNull()
+		{
+			if (WaistWidth is null &&
+				HeadSize is null &&
+				EartlerWidth is null &&
+				NeckThickness is null &&
+				HandsHeadColor is null &&
+				EyeSize is null &&
+				NarrowEyes is null &&
+				EyesAngle is null &&
+				Fatness is null &&
+				NarrowWaist is null &&
+				LegsSize is null &&
+				ArmThickness is null &&
+				WideTeeth  is null &&
+				PupilSize  is null &&
+				Scruffy is null &&
+				ColoredEartlerTips is null &&
+				DeepPupils is null &&
+				ColoredPupils is null &&
+				TailSegs is null &&
+				GeneralMelanin is null)
+				return true;
+
+			return false;
+		}
 	}
     public interface IEartlersParams : ISearchParams
     {
@@ -427,7 +462,7 @@ namespace IDFinder
 		public (float target, float weight)? Top { get; set; }
 		public (float target, float weight)? Bottom { get; set; }
 		public (BackPattern target, float weight)? Pattern { get; set; }
-		public (BackDecals target, float weight)? Type { get; set; }
+		public (string target, float weight)? Type { get; set; }	// Would be BackDecals not string, but this circumvents calling ToString() constantly to compare it.
 		public (ColorTypeEnum target, float weight)? ColorType { get; set; }
 		public (bool target, float weight)? IsColored { get; set; }
 		public (int target, float weight)? ScaleGraf { get; set; }
