3e917b1dd4e6188a6ab18edd2d85b8c3d1f1f676
Eb3yr <adamkjohnson123@gmail.com>
2024-08-21 16:34:37 +0100

    Removed errors from old code in Main entrypoint

--- IDFinder/Program.cs
+++ IDFinder/Program.cs
@@ -8,27 +8,6 @@ namespace IDFinder
 		
 		static void Main(string[] args)
 		{
-            
-            PersonalityParams pp = new();
-            ((ISearchParams)pp).AllNull();
-
-			Scavenger scav = new(0);
-
-			JsonSerializerOptions opt = new()
-			{
-				WriteIndented = true,
-				Converters =
-				{
-					new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
-				},
-				IncludeFields = true
-			};
-			string json = JsonSerializer.Serialize(scav, opt);
-			json += "\n\nBack Patterns:\n";
-			json += $"type: {scav.BackPatterns.GetType()}\n";
-			json += JsonSerializer.Serialize(scav.BackPatterns as object, opt);
-			File.WriteAllText("scav.json", json);
-
             Console.WriteLine("Done");
             Console.ReadLine();
         }
