c86b9f5f46f79e69158afbabe6780fb9a2fdb0a8
Eb3yr <adamkjohnson123@gmail.com>
2024-08-21 17:46:48 +0100

    Saving program.cs

--- IDFinder/Program.cs
+++ IDFinder/Program.cs
@@ -1,36 +1,55 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Reflection;
 
 namespace IDFinder
 {
 	internal class Program
 	{
-		
+
 		static void Main(string[] args)
 		{
-            
-            PersonalityParams pp = new();
-            ((ISearchParams)pp).AllNull();
-
-			Scavenger scav = new(0);
-
-			JsonSerializerOptions opt = new()
+			SearchParams sParams = new()
 			{
-				WriteIndented = true,
-				Converters =
-				{
-					new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
-				},
-				IncludeFields = true
+				BodyColorL = (0.5f, 1f),
+				HeadColorL = (0.5f, 1f),
+				BodyColorS = (1f, 1f),
+				HeadColorS = (1f, 1f),
+				BodyColorH = (0.16f, 0.5f),
+				HeadColorH = (0.16f, 0.5f),
+				NumberOfSpines = (40, 2f),
+				GeneralSize = (1f, 1f),
+				//ColorType = (BackTuftsAndRidges.ColorTypeEnum.Decoration, 2f),
+				//Type = ("HardBackSpikes", 1f),
+				DecorationColorH = (0.16f, 0.5f),
+				DecorationColorL = (0.5f, 0.5f),
+				DecorationColorS = (1f, 0.5f)
 			};
-			string json = JsonSerializer.Serialize(scav, opt);
-			json += "\n\nBack Patterns:\n";
-			json += $"type: {scav.BackPatterns.GetType()}\n";
-			json += JsonSerializer.Serialize(scav.BackPatterns as object, opt);
-			File.WriteAllText("scav.json", json);
+			var dt = DateTime.Now;
+			var result = Searcher.Search(0, 2871250, 30000, sParams, true);
+			Console.WriteLine("Time: " + DateTime.Now.Subtract(dt).TotalSeconds + "s");
+			//foreach (var kvp in result)
+			//	Console.WriteLine(kvp.Value + ": " + kvp.Key);
 
-            Console.WriteLine("Done");
-            Console.ReadLine();
-        }
+			Console.WriteLine("\n\n\n\n");
+			Scavenger scav;
+			List<int> final = [];
+			foreach (var kvp in result)
+			{
+				scav = new(kvp.Value);
+				if (scav.Colors.BodyColor.H > 0.18f || scav.Colors.BodyColor.H < 0.14f)
+					continue;
+				if (scav.Colors.BodyColor.S < 0.8f)
+					continue;
+				if (Math.Abs(scav.Colors.BodyColor.L - 0.5f) > 0.2f)
+					continue;
+				final.Add(kvp.Value);
+			}
+			foreach (var i in final)
+				Console.WriteLine(i);
+
+			Console.WriteLine("Done");
+			Console.ReadLine();
+		}
 	}
 }
