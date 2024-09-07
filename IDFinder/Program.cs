16c28a3a5e09c6bfc0be2a233274435350c2da49
Eb3yr <adamkjohnson123@gmail.com>
2024-09-07 17:35:29 +0100

    Update program.cs

--- IDFinder/Program.cs
+++ IDFinder/Program.cs
@@ -8,40 +8,54 @@ namespace IDFinder
 	{
 		static void Main(string[] args)
 		{
-			SearchParams sParams = new()
+			SearchParams sParams;// = new()
+			 //{
+			 //	Aggression = (1f, 1f),
+			 //	Dominance = (1f, 1f),
+			 //	Energy = (1f, 1f),
+			 //	Nervous = (1f, 1f),
+			 //	Bravery = (1f, 1f),
+			 //	Sympathy = (0f, 1f),
+			 //	BlockingSkill = (1f, 1f),
+			 //	DodgeSkill = (1f, 1f),
+			 //	MeleeSkill = (1f, 1f),
+			 //	MidRangeSkill = (1f, 1f),
+			 //	ReactionSkill = (1f, 1f),
+			 //};
+			 //sParams = new()
+			 //{
+			 //	H = (0.5208333f, 1f),
+			 //	S = (0.05882353f, 1f),
+			 //	L = (0.73333335f, 1f)
+			 //};
+			 //sParams = new ScavParams()
+			 //{
+			 //	EyeColorH = (1f, 1f),
+			 //	HeadColorH = (0.5f, 1f),
+			 //	DeepPupils = (true, 1f)
+			 //};
+			sParams = new ScavParams()
 			{
-				Aggression = (1f, 1f),
-				Dominance = (1f, 1f),
-				Energy = (1f, 1f),
-				Nervous = (1f, 1f),
-				Bravery = (1f, 1f),
-				Sympathy = (0f, 1f),
-				BlockingSkill = (1f, 1f),
-				DodgeSkill = (1f, 1f),
-				MeleeSkill = (1f, 1f),
-				MidRangeSkill = (1f, 1f),
-				ReactionSkill = (1f, 1f),
-			};
-			sParams = new()
-			{
-				H = (0.5208333f, 1f),
-				S = (0.05882353f, 1f),
-				L = (0.73333335f, 1f)
-			};
-			sParams = new()
-			{
-				L = (0.5f, 1f)
+				DecorationColorL = (0.5f, 1f),
+				DecorationColorS = (1f, 1f),
+				NumberOfSpines = (40, 1f),
+				Pattern = (BackDecals.BackPattern.DoubleSpineRidge, 0.1f),
+				ColorType = (BackTuftsAndRidges.ColorTypeEnum.Decoration, 1f),
+				GeneralSize = (1f, 0.5f)
 			};
 			var dt = DateTime.Now;
 			IEnumerable<KeyValuePair<float, int>> result;// = Searcher.SearchThreaded(0, 2871250, 300, 12, sParams, true, false);
 			var ts = TimeSpan.Zero;
 			
 			dt = DateTime.Now;
-			result = Searcher.SearchThreaded(0, int.MaxValue / 4, 32, 12, sParams, true, true);
+			result = Searcher.SearchThreaded(int.MinValue, int.MaxValue, 1024, 12, sParams, true, true);
 			//Searcher.Search(0, 1000000, 4, sParams, true);
 			ts += DateTime.Now.Subtract(dt);
 
 			File.WriteAllLines("results.txt", result.Select(res => $"{res.Value}: {res.Key}"));
+
+			foreach (var i in result)
+				Console.WriteLine(i.Value + ": " + i.Key);
 
 			Console.WriteLine("Time: " + ts.TotalSeconds + "s");
 
@@ -51,6 +65,4 @@ namespace IDFinder
 	}
 }
 
-// TODO: Add UseDetailColor to IScavBackPatternsParams
-// TODO: Rework & logging to use floats rather than ints, and update the float periodically in the if( (i & 127) == 0) statements. Maybe bump them up to 255 or even 511. Under the heaviest loads these should be updating multiple times a second - ideally 10x or more. 
-// TODO: !! Verify Abort's duration is long enough that everything terminates. Not just for my processor, but also for slow ones.
+// NOTE: If you want anything BUT a target, use a negative weight. 
