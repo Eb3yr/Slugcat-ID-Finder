b2c31f7a3ff0487571e7a9336ed24cbcc3556dd8
Eb3yr <adamkjohnson123@gmail.com>
2024-09-16 02:38:17 +0100

    Component hell: -Dropdowns added, both generic and string and enum implementations. -Tri-state checkbox from prev project -SliderInput mostly finished, still lacking some polish but it has been ENDLESSLY frustrating. -CSS for SliderInput

---
+++ IDFinder-App/Custom.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using System.Drawing;
+
+namespace IDFinder_App
+{
+    public static class Custom
+    {
+        public static float InverseLerp(float left, float right, float val)
+        {
+			return float.Clamp((val - left) / (right - left), 0f, 1f);
+		}
+    }
+}
