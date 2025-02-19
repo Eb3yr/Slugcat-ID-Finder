d91965d9c4a295541d03f3fc6a82d51bf0d51919
Eb3yr <Eb3yr123@gmail.com>
2025-02-19 21:11:58 +0000

    Writing to file

--- IDFinder-App/Program.cs
+++ IDFinder-App/Program.cs
@@ -10,8 +10,8 @@ builder.Services.AddServerSideBlazor();
 
 builder.WebHost.UseStaticWebAssets();
 builder.Services.AddTransient<IIndexState, IndexState>();   // https://learn.microsoft.com/en-us/aspnet/core/blazor/fundamentals/dependency-injection?view=aspnetcore-8.0 
-// no. Use some other kind of singleton pattern, services reset when the page ("app") resets. Regular dependency injection here won't work.
-
+															// no. Use some other kind of singleton pattern, services reset when the page ("app") resets. Regular dependency injection here won't work.
+builder.Services.AddTransient<IWriter, Writer>();
 var app = builder.Build();
 
 //if (!app.Environment.IsDevelopment())	// WILL BREAK LOCALHOST
@@ -30,3 +30,28 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
+
+
+namespace IDFinder_App
+{
+	public interface IWriter
+	{
+		abstract Task WriteToFile(string fileName, IEnumerable<string> strs);
+	}
+	public class Writer : IWriter
+	{
+		public Task WriteToFile(string fileName, IEnumerable<string> strs)
+		{
+			Task task = new(() =>
+			{
+				using (StreamWriter sw = new StreamWriter(fileName, false))
+				{
+					foreach (string str in strs)
+						sw.WriteLine(str);
+				}
+			});
+			task.Start();
+			return task;
+		}
+	}
+}
