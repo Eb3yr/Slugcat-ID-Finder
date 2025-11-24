a165b8bd2e67bb5ce12c4c2d5ef233ab0a9f987e
Eb3yr <Eb3yr123@gmail.com>
2025-11-24 14:20:25 +0000

    Tweaks & Experiments for HPC. Primarily focused on working around an unknown bottleneck that occurs at high core counts. The hope is that using multiple processes will work around this, then all the results can be aggregated at the end by a master process.

--- IDFinder/Program.cs
+++ IDFinder/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Reflection;
+using System.Diagnostics;
 
 namespace IDFinder
 {
@@ -9,34 +10,40 @@ namespace IDFinder
 		static void Main(string[] args)
 		{
 			SearchParams sParams;
+			sParams = new()
+			{
+				Scruffy = (1f, 1f),
+				HeadColorL = (0.5f, 1f),
+				HeadColorS = (1f, 1f),
+				ReactionSkill = (1f, 0.2f),
+				NumberOfSpines = (40, 0.25f),
+				IsColored = (true, 0.4f),
+			};
 			//sParams = new ScavParams()
 			//{
-			//	NumberOfSpines = (0, 1f),
-			//	ArmThickness = (0f, 1f),
-			//	NeckThickness = (0f, 1f),
-			//	WaistWidth = (1f, 1f)
+			//	NumberOfSpines = (2, 1f),
+			//	Pattern = (BackDecals.BackPattern.RandomBackBlotch, 10f),
+			//	DecorationColorS = (1f, 1f),
 			//};
-			sParams = new ScavParams()
+
+			EventHandler<SearchProgressEventArgs>? progEvent = null;
+			progEvent += (sender, args) =>
 			{
-				NumberOfSpines = (2, 1f),
-				Pattern = (BackDecals.BackPattern.RandomBackBlotch, 10f),
-				DecorationColorS = (1f, 1f),
+				Console.WriteLine($"{float.Round(args.Progress, 1)}%");
 			};
-			var dt = DateTime.Now;
-			IEnumerable<KeyValuePair<float, int>> result;// = Searcher.SearchThreaded(0, 2871250, 300, 12, sParams, true, false);
-			var ts = TimeSpan.Zero;
+
+			IEnumerable<KeyValuePair<float, int>> result;
 			
-			dt = DateTime.Now;
-			result = Searcher.SearchThreaded(0, int.MaxValue / 64, 24, 12, sParams, true);
-			//Searcher.Search(0, 1000000, 4, sParams, true);
-			ts += DateTime.Now.Subtract(dt);
+			Stopwatch sw = Stopwatch.StartNew();
+			result = Searcher.SearchThreaded(0, int.MaxValue / 64, 32, 12, sParams, true, progressEventHandler: progEvent, progressInterval: 5000);
+			sw.Stop();
 
 			File.WriteAllLines("results.txt", result.Select(res => $"{res.Value}: {res.Key}"));
 
 			foreach (var i in result)
 				Console.WriteLine(i.Value + ": " + i.Key);
 
-			Console.WriteLine("Time: " + ts.TotalSeconds + "s");
+			Console.WriteLine($"Time: {sw.ElapsedMilliseconds / 1000f}s");
 
 			Console.WriteLine("Done");
 			Console.ReadLine();
